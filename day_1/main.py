bfeb60800d449d0a5b6b266e34249279f68b75ba
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2022-12-02 05:38:56 +0700

    Create main.py

---
+++ day_1/main.py
@@ -0,0 +1,18 @@
+s = open("input.txt")
+g = s.read()
+s.close()
+
+nice = []
+
+for x in g:
+	h = x.strip().split("\n")
+	nice.append([int(x) for x in h])
+	
+all = []
+for b in nice:
+	value = 0
+	for x in b:
+		value += x
+		
+	all.append(value)
+print(sorted(all,reverse=True))
